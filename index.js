5a59e36f7cf9e7cd0650290c3accaeff8b7ce9c5
Shambhavi Rai <112109952+luna1902@users.noreply.github.com>
2024-01-10 18:38:33 +0530

    https server created

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const fs = require("fs");
+const express = require('express')
+const app = express()
+const port = 3000
 
-function callbackfn(err,data)
+function handleFirstRequest(req, res)
 {
-    console.log(data);
+    var calculated= Sum(100);
+    console.log(calculated);
+    var answer = "the sum is " + calculated;
+    res.send(answer);
 }
 
-fs.readFile("a.txt","utf-8",callbackfn);
+app.get('/', handleFirstRequest)
+
+function started() {  
+        console.log('Example app listening on port ${port}' )
+    
+}
+ 
+
+app.listen(port, started)
+
+
+function Sum(Counter)
+{
+    var i,s=0;
+    for(i=0;i<=Counter;i++)
+    {
+      s=s+i;
+    }
+    return s;
+}
 
 
 
-// function Sum(Counter)
+// const fs = require("fs");
+// const express = re
+// // fs is an external library that has all the logic required to read from an external file and write into it
+// function callbackfn(err,data)
 // {
-//     var i,s=0;
-//     for(i=0;i<=Counter;i++)
-//     {
-//       s=s+i;
-//     }
-//     return s;
+//     console.log(data);
 // }
 
-// var calculated= Sum(100);
-// console.log(calculated);
+// fs.readFile("a.txt","utf-8",callbackfn);
+// // here, reading from a file "a.txt" and logging data using callbackfn ---utf is the encoding
+
+
+
+
